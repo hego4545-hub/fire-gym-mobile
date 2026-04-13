877223de9341d274f6a7b1c50614dbe883b1a936
hego4545-hub <hego4545@gmail.com>
2026-04-13 02:22:13 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Service Worker for Fire Gym Mobile
+const CACHE_NAME = 'fire-gym-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
